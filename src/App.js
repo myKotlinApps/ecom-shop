bf19a0fe0c590824a01a625e8517c864251502fb
unknown <myKotlinApps@gmail.com>
2020-12-17 17:39:16 +0330

    header added to website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import react from 'react';
+import React from 'react';
 import './App.css';
+
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component.jsx'
+import Header from './components/header/header.component.jsx';
 
 import {Route ,Switch} from "react-router-dom";
 
@@ -11,6 +13,7 @@ import {Route ,Switch} from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
           <Route exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage}/>
